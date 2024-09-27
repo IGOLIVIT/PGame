b429fae4f93eb7d1223121c302050db41ebec9df
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-09-27 12:19:27 +0300

    Initial Commit

---
+++ PGame/PGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PGameApp.swift
+//  PGame
+//
+//  Created by IGOR on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
